fe97662bfed906e82648520d06a047a0a2edc881
Mehmed Tarık Çamlı <mehmedtarkc@gmail.com>
2023-09-28 17:00:44 +0300

    Eşini Bul Oyunu Finish

--- EsiniBulOyunu/Form1.Designer.cs
+++ EsiniBulOyunu/Form1.Designer.cs
@@ -28,30 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             pnlKartlar = new Panel();
+            gboYeniOyun = new GroupBox();
+            rb5 = new RadioButton();
+            rb4 = new RadioButton();
+            rb3 = new RadioButton();
+            rb2 = new RadioButton();
+            rb1 = new RadioButton();
+            btnOyunuBaslat = new Button();
+            menuStrip1 = new MenuStrip();
+            oyunToolStripMenuItem = new ToolStripMenuItem();
+            btnYeniOyun = new ToolStripMenuItem();
+            gboYeniOyun.SuspendLayout();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // pnlKartlar
             // 
-            pnlKartlar.Location = new Point(12, 12);
+            pnlKartlar.Anchor = AnchorStyles.None;
+            pnlKartlar.BackColor = Color.Transparent;
+            pnlKartlar.Location = new Point(12, 38);
             pnlKartlar.Name = "pnlKartlar";
-            pnlKartlar.Size = new Size(600, 600);
+            pnlKartlar.Size = new Size(600, 637);
             pnlKartlar.TabIndex = 0;
+            // 
+            // gboYeniOyun
+            // 
+            gboYeniOyun.Anchor = AnchorStyles.None;
+            gboYeniOyun.BackColor = Color.Transparent;
+            gboYeniOyun.Controls.Add(rb5);
+            gboYeniOyun.Controls.Add(rb4);
+            gboYeniOyun.Controls.Add(rb3);
+            gboYeniOyun.Controls.Add(rb2);
+            gboYeniOyun.Controls.Add(rb1);
+            gboYeniOyun.Controls.Add(btnOyunuBaslat);
+            gboYeniOyun.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            gboYeniOyun.Location = new Point(155, 206);
+            gboYeniOyun.Name = "gboYeniOyun";
+            gboYeniOyun.Size = new Size(333, 361);
+            gboYeniOyun.TabIndex = 0;
+            gboYeniOyun.TabStop = false;
+            gboYeniOyun.Text = "Yeni Oyun";
+            // 
+            // rb5
+            // 
+            rb5.AutoSize = true;
+            rb5.Location = new Point(94, 247);
+            rb5.Name = "rb5";
+            rb5.Size = new Size(116, 36);
+            rb5.TabIndex = 5;
+            rb5.Text = "Çok Zor";
+            rb5.UseVisualStyleBackColor = true;
+            // 
+            // rb4
+            // 
+            rb4.AutoSize = true;
+            rb4.Location = new Point(94, 195);
+            rb4.Name = "rb4";
+            rb4.Size = new Size(68, 36);
+            rb4.TabIndex = 4;
+            rb4.Text = "Zor";
+            rb4.UseVisualStyleBackColor = true;
+            // 
+            // rb3
+            // 
+            rb3.AutoSize = true;
+            rb3.Checked = true;
+            rb3.Location = new Point(94, 146);
+            rb3.Name = "rb3";
+            rb3.Size = new Size(78, 36);
+            rb3.TabIndex = 3;
+            rb3.TabStop = true;
+            rb3.Text = "Orta";
+            rb3.UseVisualStyleBackColor = true;
+            // 
+            // rb2
+            // 
+            rb2.AutoSize = true;
+            rb2.Location = new Point(94, 92);
+            rb2.Name = "rb2";
+            rb2.Size = new Size(89, 36);
+            rb2.TabIndex = 2;
+            rb2.Text = "Kolay";
+            rb2.UseVisualStyleBackColor = true;
+            // 
+            // rb1
+            // 
+            rb1.AutoSize = true;
+            rb1.Location = new Point(94, 38);
+            rb1.Name = "rb1";
+            rb1.Size = new Size(137, 36);
+            rb1.TabIndex = 1;
+            rb1.Text = "Çok Kolay";
+            rb1.UseVisualStyleBackColor = true;
+            // 
+            // btnOyunuBaslat
+            // 
+            btnOyunuBaslat.Location = new Point(73, 308);
+            btnOyunuBaslat.Name = "btnOyunuBaslat";
+            btnOyunuBaslat.Size = new Size(169, 47);
+            btnOyunuBaslat.TabIndex = 0;
+            btnOyunuBaslat.Text = "Oyunu Başlat";
+            btnOyunuBaslat.UseVisualStyleBackColor = true;
+            btnOyunuBaslat.Click += btnOyunuBaslat_Click;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.BackgroundImageLayout = ImageLayout.Zoom;
+            menuStrip1.Items.AddRange(new ToolStripItem[] { oyunToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(632, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // oyunToolStripMenuItem
+            // 
+            oyunToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { btnYeniOyun });
+            oyunToolStripMenuItem.Name = "oyunToolStripMenuItem";
+            oyunToolStripMenuItem.Size = new Size(48, 20);
+            oyunToolStripMenuItem.Text = "Oyun";
+            // 
+            // btnYeniOyun
+            // 
+            btnYeniOyun.Name = "btnYeniOyun";
+            btnYeniOyun.ShortcutKeys = Keys.F2;
+            btnYeniOyun.Size = new Size(147, 22);
+            btnYeniOyun.Text = "Yeni Oyun";
+            btnYeniOyun.Click += btnYeniOyun_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(625, 623);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(632, 696);
+            Controls.Add(gboYeniOyun);
             Controls.Add(pnlKartlar);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Eşini Bul Oyunu";
+            gboYeniOyun.ResumeLayout(false);
+            gboYeniOyun.PerformLayout();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Panel pnlKartlar;
+        private GroupBox gboYeniOyun;
+        private RadioButton rb5;
+        private RadioButton rb4;
+        private RadioButton rb3;
+        private RadioButton rb2;
+        private RadioButton rb1;
+        private Button btnOyunuBaslat;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem oyunToolStripMenuItem;
+        private ToolStripMenuItem btnYeniOyun;
     }
 }
